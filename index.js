e09723c7a3f165850b588bcb72a19802249765b3
siid sharma <siid123ck@gmail.com>
2021-01-30 02:27:28 +1100

    complete calculator

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const clearValue = document.querySelector(".ac");
 
 let firstValue = 0;
 let operatorValue = "";
-let waitSync = false;
+let waitSyncNext = false;
 
-
+const calculate = {
+    '%':(firstValue, secondValue)=>firstValue  % secondValue,
+    '/':(firstValue, secondValue)=>firstValue  / secondValue,
+    '*':(firstValue, secondValue)=>firstValue  * secondValue,
+    '-':(firstValue, secondValue)=>firstValue  - secondValue,
+    '+':(firstValue, secondValue)=>firstValue  + secondValue,
+    '=':(firstValue, secondValue)=>  secondValue
+}
 
 
 buttons.forEach(btn=>{
@@ -22,8 +29,14 @@ buttons.forEach(btn=>{
 })
 
 function getValue(number){
+    //replace current value if first value is entered
+    if(waitSyncNext){
+        calculatorResult.textContent=number;
+        waitSyncNext=false;
+    } else{
     const displayValue = calculatorResult.textContent;
     calculatorResult.textContent=displayValue==="0"?number:displayValue+number;
+    }
 }
 
 console.log(clearValue)
@@ -31,10 +44,12 @@ clearValue.addEventListener("click",()=>{
     calculatorResult.textContent="0";
     firstValue = 0;
     operatorValue = '';
-    waitSync=false;
+    waitSyncNext=false;
 })
 
 function dotValue(){
+    //if operator entered do not add dot value
+    if(waitSyncNext){return ;}
     if(!calculatorResult.textContent.includes(".")){
         calculatorResult.textContent = calculatorResult.textContent + ".";
     }
@@ -43,9 +58,22 @@ function dotValue(){
 function useOperator(operator){
     const currentValue = Number(calculatorResult.textContent);
     //assigning value if not 
+
+// if operator already entered skip the process
+    if(operator && waitSyncNext){
+        operatorValue = operator;
+        return ;
+    }
+
     if (!firstValue){
         firstValue = currentValue;
-    } 
+    } else {
+        console.log(firstValue, operatorValue, currentValue)
+        const calculation = calculate[operatorValue](firstValue,currentValue);
+        console.log( 'calculation', calculation )
+        calculatorResult.textContent=calculation;
+        firstValue=calculation;
+    }
+    waitSyncNext = true;
     operatorValue = operator;
-    console.log(firstValue, operatorValue);
-}
+} 
